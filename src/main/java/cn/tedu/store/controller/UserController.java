6348b6c54b4882842a42099dbae9994bdd619483
PanW <PanW-@outlook.com>
2020-08-05 21:50:52 +0800

    2020年8月5日21:50:47 PanW

--- src/main/java/cn/tedu/store/controller/UserController.java
+++ src/main/java/cn/tedu/store/controller/UserController.java
@@ -2,6 +2,7 @@ package cn.tedu.store.controller;
 
 import java.io.File;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -24,6 +25,8 @@ public class UserController extends BaseController{
 	// http://localhost:80/users/reg?username=controller&password=1234&gender=0&phone=13100131001&email=controller@qq.com
 	@RequestMapping("reg")
 	public JsonResult<Void> reg(User user) {
+		//日志
+		System.err.println("UserController.reg()");
 		iUserService.reg(user);
 		return new JsonResult<>(OK);
 	}
@@ -31,6 +34,8 @@ public class UserController extends BaseController{
 	// http://localhost:80/users/login?username=root&password=1234
 	@RequestMapping("login")
 	public JsonResult<User> login(String username, String password, HttpSession session) {
+		//日志
+		System.err.println("UserController.login()");
 		User data = iUserService.login(username, password);
 		session.setAttribute("uid", data.getUid());
 		session.setAttribute("username", data.getUsername());
@@ -44,6 +49,8 @@ public class UserController extends BaseController{
 	//响应结果：JsonResult<Void>
 	@RequestMapping("password/change")
 	public JsonResult<Void> changePassword(String oldPassword, String newPassword, HttpSession session) {
+		//日志
+		System.err.println("UserController.changePassword()");
 		//从参数session中取出登录时存入的uid和username
 		Integer uid = Integer.valueOf(session.getAttribute("uid").toString());
 		String username = session.getAttribute("username").toString();
@@ -60,6 +67,8 @@ public class UserController extends BaseController{
 	//响应结果：JsonResult<User>
 	@RequestMapping("info/show")
 	public JsonResult<User> showInfo(HttpSession session) {
+		//日志
+		System.err.println("UserController.showInfo()");
 		//从参数session中取出登录时存入的uid
 		Integer uid = Integer.valueOf(session.getAttribute("uid").toString());
 		//调用业务对象的showInfo()方法查询用户数据
@@ -75,6 +84,8 @@ public class UserController extends BaseController{
 	//响应结果：JsonResult<Void>
 	@RequestMapping("info/change")
 	public JsonResult<Void> changeInfo(User user, HttpSession session) {
+		//日志
+		System.err.println("UserController.changeInfo()");
 		// 从session中获取uid和username
 		Integer uid = getUidFromSession(session);
 		String username = getUsernameFromSession(session);
@@ -85,12 +96,22 @@ public class UserController extends BaseController{
 	}
 	
 	@PostMapping("avatar/change")
-	public JsonResult<Void> changeAvatar(MultipartFile file) throws Exception {
+	public JsonResult<Void> changeAvatar(MultipartFile file,HttpServletRequest request) throws Exception {
+		//日志
+		System.err.println("UserController.changeAvatar()");
+		//获取原始文件名(客户端设备中的文件名)
+		String originalFilename = file.getOriginalFilename();
+		System.err.println("\toriginalFilename="+originalFilename);
 		//将文件上传到哪个文件夹
-		String parent = "C:/Users/dell/Desktop/pictures";
+		String parent = request.getServletContext().getRealPath("upload");
+		System.err.println("\tupload path=" + parent);
 		//保存上传的文件时使用的文件名
-		String filename = "1";
-		String suffix = ".png";
+		String filename = "" + System.currentTimeMillis() + System.nanoTime();
+		String suffix = "";
+		int beginIndex = originalFilename.lastIndexOf(".");
+		if(beginIndex >= 1) {
+			suffix = originalFilename.substring(beginIndex);
+		}
 		String child = filename + suffix;
 		//将客户端上传的文件保存到服务器端
 		File dest = new File(parent,child);
