096866bf00b919f4b1b75d95989f593a767dd0bd
PanW-HuangR <1452612030@qq.com>
2020-08-28 16:59:22 +0800

    2020年8月28日16:59:14 PanW-HuangR

--- src/main/java/cn/tedu/store/aop/TimerAspect.java
+++ src/main/java/cn/tedu/store/aop/TimerAspect.java
@@ -1,10 +1,30 @@
 package cn.tedu.store.aop;
 
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.springframework.stereotype.Component;
 
 @Aspect
 @Component
 public class TimerAspect {
+	
+	@Around("execution(* cn.tedu.store.service.impl.*.*(..))")
+	public Object a(ProceedingJoinPoint point) throws Throwable {
+		// 记录开始时间
+	    long start = System.currentTimeMillis();
+
+	    // 相当于执行了Service中的注册或登录或其它方法
+	    // 调用的proceed()会抛出异常，必须继续抛出
+	    Object result = point.proceed();
+
+	    // 记录结束时间
+	    long end = System.currentTimeMillis();
+
+	    // 计算耗时
+	    System.err.println("耗时：" + (end - start) + "ms.");
+
+	    return result;
+	}
 	
 }
